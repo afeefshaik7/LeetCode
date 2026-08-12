c29060a02b82004217a72dc88a162d98bbd44be6
afeefshaik7 <safeef999@gmail.com>
2026-08-12 10:45:30 +0530

    Time: 11 ms (12.48%) | Memory: 21.9 MB (39.34%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int c=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==i)
+            {
+                c++;
+            }else{
+                c=i;
+                break;
+            }
+        }
+        return c;
+    }
+};
