9e2adb7034a54c2925ff7c22b4080ad17eb45c2e
afeefshaik7 <safeef999@gmail.com>
2026-08-17 15:58:40 +0530

    Time: 4 ms (30.94%) | Memory: 23.9 MB (95.06%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -3,14 +3,18 @@ public:
     vector<int>a;
     NumArray(vector<int>& nums) {
         a=nums;
+        a[0]=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            a[i]=nums[i]+a[i-1];
+        }
     }
     
     int sumRange(int left, int right) {
         int sum=0;
-        for(int i=left;i<=right;i++)
-        {
-            sum=sum+a[i];
-        }return sum;
+        if(left==0) return a[right];
+        sum=a[right]-a[left-1];
+        return sum;
     }
 };
 
