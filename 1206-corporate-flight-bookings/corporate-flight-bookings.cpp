96b6200c429b3dae51db88bab0371d36abf0c664
afeefshaik7 <safeef999@gmail.com>
2026-08-25 22:07:50 +0530

    Time: 0 ms (100.00%) | Memory: 72.2 MB (74.25%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& b, int n) {
-        vector<int>v(n,0);
+        vector<int>pre(n+1,0);
         for(int i=0;i<b.size();i++)
         {
            int c=b[i][0];
            int d=b[i][1];
-           for(int j=c-1;j<d;j++)
-           {
-            v[j]=v[j]+b[i][2];
-           }
-        }
-        return v;
+           pre[c-1]=pre[c-1]+b[i][2];
+           pre[d]=pre[d]-b[i][2];
+        }int sum=0;
+        for(int i=0;i<pre.size();i++)
+        {
+           sum=sum+pre[i];
+           pre[i]=sum;
+        }pre.pop_back();
+        return pre;
     }
 };
