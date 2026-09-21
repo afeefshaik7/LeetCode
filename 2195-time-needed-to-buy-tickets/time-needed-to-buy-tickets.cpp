7f10c2a5aab0bcb8761a1a3030322dc10644f34d
afeefshaik7 <safeef999@gmail.com>
2026-09-21 22:14:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (98.67%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {int c=0;
+        while(tickets[k]!=0)
+        {
+            for(int i=0;i<tickets.size();i++)
+            {
+                if(tickets[i]>0)
+                {
+                    c++;
+                    tickets[i]--;
+                }
+                if(tickets[k]==0)break;
+            }
+        }return c;
+    }
+};
